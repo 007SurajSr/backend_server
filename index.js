b82c598a8eed44bc33ea4caad61c35d86b35a0cd
Suraj Gupta <8700suraj@gmail.com>
2023-09-26 13:03:01 +0530

    serverstart

---
+++ index.js
@@ -0,0 +1,70 @@
+// var Express = require("express");
+// var Mongoclient = require("mongodb").MongoClient;
+// var cors = require("cors");
+// const multer = require("multer");
+
+// var app = Express();
+// app.use(cors());
+
+// var CONNECTION_STRING="mongodb+srv://8700suraj:Suraj@965411@cluster0.f4d8wsn.mongodb.net/todoapp1?retryWrites=true&w=majority ";
+
+// var DATABASENAME = "todoapp1";
+// var database;
+
+// app.listen(5038,()=>{
+//     Mongoclient.connect(CONNECTION_STRING,(error,client)=>{ 
+//         database = client.db(DATABASENAME);
+//         console.log("Mongo DB Connection is successful");
+//     })
+// })
+
+// const express = require("express");
+// const MongoClient = require("mongodb").MongoClient;
+// const cors = require("cors");
+
+// const app = express();
+// app.use(cors());
+
+// const CONNECTION_STRING = "mongodb+srv://8700suraj:Suraj@965411@cluster0.f4d8wsn.mongodb.net/todoapp1?retryWrites=true&w=majority";
+// const DATABASE_NAME = "todoapp1";
+// let database;
+
+// const port = 5038;
+
+// app.listen(port, () => {
+//   MongoClient.connect(CONNECTION_STRING, (error, client) => {
+//     if (error) {
+//       console.error("Failed to connect to MongoDB:", error);
+//       return;
+//     }
+//     database = client.db(DATABASE_NAME);
+//     console.log("MongoDB connection successful");
+//   });
+// });
+
+const express = require('express')
+const mongodb = require('mongoose')
+const cors = require ('cors')
+const UserModel = require('./models/Users')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongodb.connect("mongodb://127.0.0.1:27017/crud")
+
+ 
+ 
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World Suraj !')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.listen(3001, ()=>{
+    console.log("Server is Running")
+}) 
